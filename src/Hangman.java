9de5116a5885e155b561be7cf202c350a47e9774
Igor Kolpakov <kolpakov.igor@list.ru>
2024-05-25 00:07:12 +0300

    Доработан метод checkLetter

--- src/Hangman.java
+++ src/Hangman.java
@@ -1,40 +1,28 @@
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
 
 public class Hangman {
 
     static List<String> list = new ArrayList<>();
     static String secretWord;
-    static char[] secretWordSolvingProcess;
     static char[] secretWordMask;
     static char letter;
-    static final int wrongTriesNumber = 6;
-
-    public Hangman(List<String> list, String secretWord, char[] secretWordSolvingProcess, char[] secretWordMask, char letter) {
-        this.list = list;
-        this.secretWord = secretWord;
-        this.secretWordSolvingProcess = secretWordSolvingProcess;
-        this.secretWordMask = secretWordMask;
-        this.letter = letter;
-    }
+    static int wrongTriesNumber = 0;
 
     public static void main(String[] args) {
         Random random = new Random();
         {
-            try (FileReader reader = new FileReader("glossary.txt");
-                 Scanner scanner = new Scanner(reader);) {
+            try (FileReader reader = new FileReader("glossary.txt"); Scanner scanner = new Scanner(reader);) {
                 while (scanner.hasNextLine()) {
                     list.add(scanner.nextLine());
                 }
                 secretWord = list.get(random.nextInt(list.size())).toUpperCase();
                 System.out.println(secretWord);
 
-                gameProcess();
+                secretWordMaskCreation();
+                checkLetter();
 
 
             } catch (FileNotFoundException e) {
@@ -45,28 +33,15 @@ public class Hangman {
         }
     }
 
-    static void gameProcess() {
-        for (int i = 0; i < wrongTriesNumber; i++) {
-            checkLetter();
-            secretWordSolvingProcess = secretWordMask;
-            System.out.println(secretWordSolvingProcess);
-        }
-
-    }
-
-
     static char[] secretWordMaskCreation() {
-        int quantityOfLetters = secretWord.length();
-        secretWordMask = new char[quantityOfLetters];
-        for (int i = 0; i < quantityOfLetters; i++) {
-            secretWordMask[i] = '*';
-        }
+        secretWordMask = secretWord.toCharArray();
+        Arrays.fill(secretWordMask, '*');
         System.out.println(secretWordMask);
         return secretWordMask;
     }
 
     static char enteringLetter() {
-        System.out.println("Введите букву (кириллица)");
+        System.out.println("Введите одну букву (кириллица)");
         Scanner scanner = new Scanner(System.in);
         letter = scanner.next().charAt(0);
         if (!Character.UnicodeBlock.of(letter).equals(Character.UnicodeBlock.CYRILLIC)) {
@@ -78,16 +53,28 @@ public class Hangman {
     }
 
     static char[] checkLetter() {
-        secretWordMaskCreation();
-        enteringLetter();
-        for (int i = 0; i < secretWordMask.length; i++) {
-            if (Character.toString(secretWord.charAt(i)).equalsIgnoreCase(String.valueOf(letter))) {
-                secretWordMask[i] = letter;
+
+        while (new String(secretWordMask).contains("*")) {
+            enteringLetter();
+            System.out.println(letter);
+            if (secretWord.contains(String.valueOf(letter).toUpperCase())) {
+                for (int i = 0; i < secretWordMask.length; i++) {
+                    if (Character.toString(secretWord.charAt(i)).equalsIgnoreCase(String.valueOf(letter))) {
+                        secretWordMask[i] = letter;
+                    }
+                }
+                System.out.println(new String(secretWordMask).toUpperCase());
+            } else {
+                wrongTriesNumber++;
+                if (wrongTriesNumber < 6) {
+                    System.out.println("Ошибка: " + wrongTriesNumber + " из 6!");
+                } else {
+                    System.out.println("\nВы проиграли.\nХотите сыграть еще?");
+                }
             }
         }
-        System.out.println(secretWordMask);
+        System.out.println("\nВерно! Вы отгадали слово.\nХотите сыграть еще?");
+
         return secretWordMask;
     }
-
-
 }
